edcdcf79f2b8d7045197d0924bf4082682ac7302
closteroven <chloeouellette6@gmail.com>
2025-03-12 11:59:03 -0400

    Update load_data.py

--- lab 3/load_data.py
+++ lab 3/load_data.py
@@ -12,7 +12,7 @@ __team__ = "T051"
 
 def student_school_list(fileName: str, school: str) -> list:
     """
-    Returns a list containing a dict with the age, ID, study time, number of failures, health, absences, fall grade, and winter grade, for each student who attended the school given a csv file and a school.
+    Return a list containing a dict with the age, ID, study time, number of failures, health, absences, fall grade, and winter grade, for each student who attended the school given a csv file and a school.
     
     >>> student_school_list('student-mat.csv','GP')
     [{'ID': 1, 'Age': 18, 'StudyTime': 2.5, 'Failures': 0, 'Health': 3, 'Absences': 6, 'FallGrade': 5, 'WinterGrade': 6}, {another element}, ... ]
@@ -68,6 +68,47 @@ def student_school_list(fileName: str, school: str) -> list:
 #==========================================#
 # Place your student_health_list function after this line
 
+def student_health_list(filename: str, health: int) -> list[dict]:
+    """
+    Return a list of students, stored as dictionaries, whose health values are equivalent to health, given a csv file. Each dictionary contains a student's school, ID, age, study time, failures, absenses, fall grades and winter grades.
+    Preconditions: 1 <= health <= 5.
+    
+    >>> student_health_list('student-mat.csv', 2)
+    [ {'School': 'MS', 'ID': 20, 'Age': 20, 'StudyTime': 1.0, 'Failures': 1,'Absences': 10, 'FallGrade': 9, 'WinterGrade': 11},{another element}, …]
+    >>> student_health_list('student-mat.csv', 'health')
+    []
+    >>> student_health_list('student-mat.csv', 5)
+    [ {'School': 'GP', 'ID': 4, 'Age': 15, 'StudyTime': 3.0, 'Failures': 0,'Absences': 2, 'FallGrade': 15, 'WinterGrade': 14},{another element}, …]
+    """
+    
+    data_str = ""
+    file = open(filename, "r")
+    for ch in file:
+        data_str += ch
+    file.close()
+
+    data_str = data_str.splitlines()
+    category = data_str[0].split(',')
+    students = []
+    health_index = category.index('Health')
+    del category[health_index]
+
+    for student in data_str:
+        student = student.split(',')
+        if student[health_index] == str(health):
+            del student[health_index]
+            student_dict = {}
+            for x in range(len(student)):
+                if student[x].isdigit():
+                    student_dict[category[x]] = int(student[x])
+                else:
+                    try:
+                        student_dict[category[x]] = float(student[x])
+                    except:
+                        student_dict[category[x]] = student[x]
+            students += [student_dict]
+
+    return students
 
 #==========================================#
 # Place your student_age_list function after this line
