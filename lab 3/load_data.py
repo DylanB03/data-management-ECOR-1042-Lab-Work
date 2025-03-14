a79c7136d5596d0ca4c48a6579ea7b77564ea2d8
Cameron <cmacgill02@gmail.com>
2025-03-13 22:39:03 -0400

    Updated load_data to have failures function, 3/4 individual functions done now.

--- lab 3/load_data.py
+++ lab 3/load_data.py
@@ -112,8 +112,72 @@ def student_health_list(filename: str, health: int) -> list[dict]:
 
 #==========================================#
 # Place your student_age_list function after this line
+def student_failures_list(filename: str, failures: int) -> list:
+    """
+     Return a list of students, each of which is a dictionary with the given number of failures.
+    If there is no student with the intended number of failures, returns empty list.
+
+     >>> student_failures_list('student-mat.csv', 0)
+    [{'School': 'GP', 'ID': 201, 'Age': 18, 'StudyTime': 7.0, 'Health': 3,
+      'Absences': 7, 'FallGrade': 12, 'WinterGrade': 13},
+     {... more students with 0 failures ...}]
+
+    >>> student_failures_list('student-mat.csv', 2)
+    [{'School': 'GP', 'ID': 125, 'Age': 17, 'StudyTime': 3.0, 'Health': 4,
+      'Absences': 5, 'FallGrade': 10, 'WinterGrade': 9},
+     {... more students with 2 failures ...}]
+
+    >>> student_failures_list('student-mat.csv', 220)
+    []
+    """
+    result_list = []    #the main list to be returned at end of function
+
+    with open(filename, 'r') as file:
+
+        #reads the first line only to get the headers
+        header_line = None
+        for line in file:
+            header_line = line.strip()
+            break
+
+        headers = header_line.split(",")
+
+        # reads all the next lines after the initial header, extracts the info
+        for line in file:
+            line = line.strip()
+            #skip empty line
+            if not line:
+                continue
+
+            values = line.split(",")
+
+            # find failures col pos = index
+            failures_index = headers.index("Failures")
+
+            #change them to ints
+            current_failures = int(values[failures_index])
+
+            # cont if row failures equivalent to intended function input param
+            if current_failures == failures:
+                #make a dictionary for this case
+                student_dict = {}
+                for i, h in enumerate(headers):
+                    if h == "Failures":
+                        # Does not include failures column (handles ignore that redundant data)
+                        continue
+                    if h in ["ID", "Age", "Health", "Absences",
+                             "FG", "WG"]:
+                        student_dict[h] = int(values[i])
+                    elif h == "ST":
+                        student_dict[h] = float(values[i])
+                    else:
+                        student_dict[h] = values[i]
+
+                # Adds new student dictionary to the list of results
+                result_list.append(student_dict)
 
 
+    return result_list
 #==========================================#
 # Place your student_failures_list function after this line
 
