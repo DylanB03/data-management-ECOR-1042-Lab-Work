4b91884217cfa889ae20f7caabc2421bffd8766c
DylanB03 <dylanbutz@hotmail.com>
2025-03-17 17:38:27 -0400

    added lab 4

---
+++ lab 4/test3.py
@@ -0,0 +1,36 @@
+# ECOR 1042 Lab 4 - Individual submission for test3 function
+
+# import load_data module here
+
+# Update "" with your name (e.g., Cristina Ruiz Martin)
+__author__ = ""
+
+# Update "" with your student number (e.g., 100100100)
+__student_number__ = ""
+
+# Update "" with your team (e.g. T-102, use the notation provided in the example)
+__team__ = ""
+
+#==========================================#
+# Do not modify the code alreayd provided.
+
+
+def test_return_correct_dict_inside_list() -> list[int]:
+    # Complete the function with your test cases
+
+    # test that student_school_list returns a correct dictionary inside the list (3 different test cases required)
+
+    # test that student_age_list returns a correct dictionary inside the list  (3 different test cases required)
+
+    # test that student_health_list returns a correct dictionary inside the list  (3 different test cases required)
+
+    # test that student_failures_list returns a correct dictionary inside the list (3 different test cases required)
+
+    # test that load_data returns a correct dictionary inside the list (6 different test cases required)
+
+    # test that add_average returns a lcorrect dictionary inside the list  (3 different test cases required)
+
+    # return the a list with the number of tests that passed and the number that failed
+
+
+# Do NOT include a main script in your submission
