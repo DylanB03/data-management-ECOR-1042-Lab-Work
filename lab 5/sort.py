a5ce70404cd86a4bb80d871ff0f7d1d568b1540f
Arman-r17 <arman.rahmani1711@gmail.com>
2025-03-28 11:34:59 -0400

    added age

--- lab 5/sort.py
+++ lab 5/sort.py
@@ -9,7 +9,42 @@ __team__ = "T-051"
 
 #==========================================#
 # Place your sort_students_age_bubble function after this line
+def sort_students_age_bubble(students: list[dict], rank: str) -> str:
+    """ 
+    Returns a string of a list of students' dictionaries, bubble sorted in ascending or descending order based off age. If the inputted list is empty, the function returns the string “Empty list.”. If “Age” is not a key in the dictionary, the function returns the string “List not sorted. Age key not present.”.
+    
+    >>> a = []
+    >>> sort_students_age_bubble(a, "A")
+    'Empty list.'
+    
+    >>> a = [{"Age":15,"School":"GP"},{"Age":19,"School":"GP"}]
+    >>> sort_students_age_bubble(a, "D")
+    'List sorted.'
+    
+    >>> a = [{"StudyTime":20}, {"Studytime":12}]
+    >>> sort_students_age_bubble(a, "D")
+    'List not sorted. Age key not present.'
+    """
+    
+    if not students:
+        return "Empty list."
 
+    if not all("Age" in student for student in students):
+        return "List not sorted. Age key not present."
+
+    if rank == "A":
+        for i in range(len(students)):
+            for j in range(len(students) - i - 1):
+                if students[j]["Age"] > students[j + 1]["Age"]:
+                    students[j], students[j + 1] = students[j + 1], students[j]
+
+    elif rank == "D":      
+        for i in range(len(students)):
+            for j in range(len(students)- i - 1):
+                if students[j]["Age"] < students[j + 1]["Age"]:
+                    students[j], students[j + 1] = students[j + 1], students[j]
+
+    return "List sorted."
 
 #==========================================#
 # Place your sort_students_time_selection function after this line
